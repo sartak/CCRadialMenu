42387f0d42f2dd15c9767759d3f46f2206c67d51
Shawn M Moore <code@sartak.org>
2012-06-21 17:57:16 -0400

    Import implementation

---
+++ CCRadialMenu.h
@@ -0,0 +1,16 @@
+#import <GameKit/GameKit.h>
+#import "cocos2d.h"
+
+@interface CCMenu (missing) {
+}
+-(CCMenuItem *) itemForTouch: (UITouch *) touch;
+@end
+
+@interface CCRadialMenu : CCMenu {
+    float radius_;
+}
+
++(id) radialMenuWithArray:(NSArray *)items radius:(float)radius;
+-(id) initRadialMenuWithArray:(NSArray *)items radius:(float)radius;
+
+@end
---
+++ CCRadialMenu.m
@@ -0,0 +1,54 @@
+#import "CCRadialMenu.h"
+
+@implementation CCRadialMenu
+
++(id) radialMenuWithArray:(NSArray *)items radius:(float)radius {
+    return [[[self alloc] initRadialMenuWithArray:items radius:radius] autorelease];
+}
+-(id) initRadialMenuWithArray:(NSArray *)items radius:(float)radius;
+{
+    if( (self=[super initWithArray:items]) ) {
+        radius_ = radius;
+    }
+    return self;
+}
+
+-(CCMenuItem *) itemForTouch: (UITouch *) touch
+{
+    CGPoint touchLocation = [touch locationInView: [touch view]];
+    touchLocation = [[CCDirector sharedDirector] convertToGL: touchLocation];
+    
+    CGPoint location = [self convertToNodeSpace:touchLocation];
+    
+    float distance = sqrt( location.x*location.x + location.y*location.y );
+
+    if (distance >= radius_ * 0.5 && distance <= radius_ * 1.5) {
+        int count = [children_ count];
+        double factor = (2 * 3.14) / count;
+
+        double theta = atan2(location.x, location.y);
+        /* I use [0, 2pi) for layout, but atan2 returns [-pi, pi) */
+        if (theta < 0) {
+            theta += 2 * M_PI;
+        }
+
+        /* find closest character */
+        int i = theta/factor + 0.5;
+
+        /* don't let them tap halfway across the circle if there are few kana */
+        double angle_distance = fabs(factor * i - theta);
+        if (angle_distance < M_PI/5) {
+            /* tapping at 11 o'clock is like tapping at 1 o'clock */
+            if (i == count) { i = 0; }
+            CCMenuItem* item = [children_ objectAtIndex:i];
+            if ( [item visible] && [item isEnabled] ) {
+                return item;
+            }
+        }
+    }
+
+    /* delegate for the correct answer that moves, etc */
+    return [super itemForTouch:touch];
+}
+
+@end
