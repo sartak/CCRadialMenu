797a78915b883aa68ebaee3c42b2244d766962a9
Shawn M Moore <code@sartak.org>
2012-06-21 18:01:15 -0400

    Tidy, genericize comments

--- CCRadialMenu.m
+++ CCRadialMenu.m
@@ -5,9 +5,10 @@
 +(id) radialMenuWithArray:(NSArray *)items radius:(float)radius {
     return [[[self alloc] initRadialMenuWithArray:items radius:radius] autorelease];
 }
+
 -(id) initRadialMenuWithArray:(NSArray *)items radius:(float)radius;
 {
-    if( (self=[super initWithArray:items]) ) {
+    if ( (self=[super initWithArray:items]) ) {
         radius_ = radius;
     }
     return self;
@@ -17,9 +18,9 @@
 {
     CGPoint touchLocation = [touch locationInView: [touch view]];
     touchLocation = [[CCDirector sharedDirector] convertToGL: touchLocation];
-    
+
     CGPoint location = [self convertToNodeSpace:touchLocation];
-    
+
     float distance = sqrt( location.x*location.x + location.y*location.y );
 
     if (distance >= radius_ * 0.5 && distance <= radius_ * 1.5) {
@@ -32,10 +33,10 @@
             theta += 2 * M_PI;
         }
 
-        /* find closest character */
+        /* find closest item */
         int i = theta/factor + 0.5;
 
-        /* don't let them tap halfway across the circle if there are few kana */
+        /* don't let them tap halfway across the circle if there are few items */
         double angle_distance = fabs(factor * i - theta);
         if (angle_distance < M_PI/5) {
             /* tapping at 11 o'clock is like tapping at 1 o'clock */
@@ -47,7 +48,7 @@
         }
     }
 
-    /* delegate for the correct answer that moves, etc */
+    /* delegate for tems that move, etc */
     return [super itemForTouch:touch];
 }
 
